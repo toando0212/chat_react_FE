53a789ade6f03a96c4d6501d7b434f87dae423c7
toan0212 <toando0212@gmail.com>
2025-09-03 15:51:44 +0700

    chanrg

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 /// <reference types="node" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
